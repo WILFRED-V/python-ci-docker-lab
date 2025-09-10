48a514660d49c4147f0dbe8e33bfce638a92c0bc
WILFRED-V <wilfredv@gmail.com>
2025-09-10 15:36:32 +0530

    First commit - upload my project

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import app
+
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+    assert app.add(2, 3) == 5
